d9f7375c3e6974f3286f23dff893d8bf5db794a6
mwyrebski <mwyrebski@users.noreply.github.com>
2019-04-16 23:42:42 +0200

    Fix tests for Factorial and Fibonnaci

--- Excercises.Tests/Factorial.Tests.fs
+++ Excercises.Tests/Factorial.Tests.fs
@@ -4,12 +4,13 @@ open Excercises
 open Xunit
 
 [<Theory>]
-[<InlineData(0, 1)>]
+[<InlineData(1, 0)>]
 [<InlineData(1, 1)>]
 [<InlineData(2, 2)>]
 [<InlineData(6, 3)>]
 [<InlineData(24, 4)>]
 [<InlineData(120, 5)>]
-let ``Test Factorial calculation`` input expected =
+[<InlineData(720, 6)>]
+let ``Test Factorial calculation`` expected input =
     let actual = Factorial.calc input
     Assert.Equal(expected, actual)
--- Excercises.Tests/Fibonnaci.Tests.fs
+++ Excercises.Tests/Fibonnaci.Tests.fs
@@ -4,12 +4,15 @@ open Excercises
 open Xunit
 
 [<Theory>]
-[<InlineData(0, 1)>]
+[<InlineData(0, 0)>]
 [<InlineData(1, 1)>]
-[<InlineData(3, 2)>]
-[<InlineData(5, 3)>]
-[<InlineData(8, 4)>]
-[<InlineData(13, 5)>]
-let ``Test Fibonnaci calculation`` input expected =
+[<InlineData(1, 2)>]
+[<InlineData(2, 3)>]
+[<InlineData(3, 4)>]
+[<InlineData(5, 5)>]
+[<InlineData(8, 6)>]
+[<InlineData(13, 7)>]
+[<InlineData(21, 8)>]
+let ``Test Fibonnaci calculation`` expected input =
     let actual = Fibonnaci.calc input
     Assert.Equal(expected, actual)
